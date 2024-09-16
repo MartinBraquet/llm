f6c8f1154c66f314bd4f06f87b26a5b71bc7df35
MartinBraquet <martin.braquet@gmail.com>
2024-09-16 18:52:08 +0200

    Fix

--- llm/gpu.py
+++
@@ -1,9 +0,0 @@
-import torch
-import torch_directml
-
-device = torch_directml.device()
-
-tensor1 = torch.tensor([1]).to(device)
-tensor2 = torch.tensor([2]).to(device)
-dml_algebra = tensor1 + tensor2
-print(dml_algebra)
