5c49c9e876cc40e66585622d563cb96729d8432c
MartinBraquet <martin.braquet@gmail.com>
2024-09-25 15:50:02 +0200

    Fix test reproducibility

--- llm/tests/test_sample.py
+++ llm/tests/test_sample.py
@@ -1,5 +1,9 @@
 from pathlib import Path
 from unittest import TestCase
+from unittest.mock import patch
+
+import numpy as np
+import torch
 
 from llm import BASE_DIR
 from llm.sample import generate_text, Sampler
@@ -7,6 +11,19 @@ from llm.sample import generate_text, Sampler
 DIR = Path(__file__).parent
 
 
+def patch_multinomial_sampling(probs, num_samples):
+    """
+    Seeded torch.multinomial is platform-dependent, so we can't use it for reproducibility in unit testing
+    Using np.random.multinomial allows for reproducibility. But since is less efficient (need to share memory between
+    CPU and GPU), we stick to torch.multinomial in production.
+    """
+    probs = np.array(probs[0]).astype(np.float64)
+    probs /= probs.sum()
+    idx_next = torch.asarray([[np.argmax(np.random.multinomial(num_samples, probs, size=1)[0])]])
+    return idx_next
+
+
+@patch('torch.multinomial', patch_multinomial_sampling)
 class TestSample(TestCase):
 
     def test_sample(self):
